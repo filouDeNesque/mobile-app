9a826416c7e631bca3e16d2fa9e48f503f3f3996
Paul cuquemy <p.cuquemy@yahoo.com>
2020-10-25 17:17:12 +0100

    btn menu

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-var menu = document.getElementsByClassName("ul-nav");
-console.log(menu)
+var menu = document.getElementById("myBtn");
+var menu_btn = document.getElementsByClassName("menu-btn")
+var color_btn = document.getElementsByClassName("color-btn")
+var menuListe = document.getElementsByClassName("menuListe");
 
-addEventListener
+console.log(menuListe)
+
+menu.addEventListener("click", function () {
+    if (color_btn.length === 0) {
+        for (var i = 0; i < menuListe.length; i++) {
+            menuListe[i].classList.add("mobile-active")
+        }
+        for (var i = 0; i < menu_btn.length; i++) {
+            menu_btn[i].classList.add("color-btn")
+        }
+    }
+    else {
+        for (var i = 0; i < menuListe.length; i++) {
+            menuListe[i].classList.remove("mobile-active")
+        }
+        for (var i = 0; i < menu_btn.length; i++) {
+            menu_btn[i].classList.remove("color-btn")
+        }
+    }
+})
